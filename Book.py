7ac88e5e50606d10a9ca22a61d5e95b2dd66800d
Enny <46939885+EnnyGit@users.noreply.github.com>
2020-05-14 22:18:53 +0200

    book class

---
+++ Book.py
@@ -0,0 +1,20 @@
+class Person():  
+    """"This is a book class"""
+    def __init__(self, author, country, imageLink, language, link, pages, 
+    title, year):
+        self.author = number
+        self.gender = gender
+        self.nameSet = nameSet
+        self.givenName = givenname
+        self.surname = surname
+        self.streetAddress = streetaddress
+        self.zipCode = zipcode
+        self.city = city
+        self.emailAddress = emailAddress
+        self.username = username
+        self.telephonenumber = telephonenumber
+    
+    #def writeToDatabase():
+        
+    def Sleep(self):
+        print(self.username + " slaapt")
